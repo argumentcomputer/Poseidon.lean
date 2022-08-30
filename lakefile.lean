dac351bb4b4e290c5f6f3bfb978e71ebbf273443
DanielRrr <daniel.rogozin@serokell.io>
2022-08-30 15:57:34 +0400

    add the mathbin dependency

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,9 @@
 import Lake
 open Lake DSL
 
-package Poseidon.lean {
-  -- add configuration options here
-}
+package Poseidon 
+
+lean_lib Poseidon
+
+require mathlib3port from git
+   "https://github.com/leanprover-community/mathlib3port.git"@"a46edb9fec446cdef642ca202ea14eff6f38c64e"
