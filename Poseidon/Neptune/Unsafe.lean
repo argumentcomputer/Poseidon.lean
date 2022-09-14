335204e4469182c8465045ef6c430059a388e72b
DanielRrr <daniel.rogozin@serokell.io>
2022-09-14 18:52:15 +0300

    the P_hash function with Matrix and Vector data types

--- Poseidon/Neptune/Unsafe.lean
+++ Poseidon/Neptune/Unsafe.lean
@@ -2,9 +2,12 @@ import Poseidon.Util.Zmod
 import Poseidon.Util.Matrix
 import Poseidon.Util.Util
 
+import Mathlib.Init.Algebra.Order
+import Mathlib.Init.Data.Nat.Lemmas
+
 namespace Unsafe
 
-open Zmod Matrix Util
+open Zmod Matrix Util Nat
 
 variable (p t : Nat)
 
@@ -34,4 +37,75 @@ def P_perm (R_f R_p : Nat) (S_box' : Zmod p → Zmod p) (c a : Vector (Zmod p) t
     (iterate R_f (R_f_round p t S_box' c MDS)) ((iterate R_p (R_p_round p t S_box' c MDS'))
       ((iterate R_f $ R_f_round p t S_box' c MDS) a))
 
+/- Adding an `r`-chunk to the state. -/
+def add_to_state (r : Nat) (m : Vector (Zmod p) r) 
+  (a : Vector (Zmod p) t) : Vector (Zmod p) t :=
+  λ i => dite ((i : Nat) < r) (λ h => a i + m (castLt i h)) (λ _ => a i)
+
+def fin_coercion (ho : o < r + cap) : Fin o → Fin (r + cap) :=
+  λ (i : Fin o) => 
+    (⟨(i : Nat), lt_of_le_of_lt (le_of_lt i.2) ho⟩ : Fin (r + cap))
+
+lemma helper_1 (d r cap : Nat) (j : Fin (d * r + (r + cap))) :
+  ↑j + r < (d + 1) * r + (r + cap) := by
+    have h1 : d.succ * r + (r + cap) = d * r + (r + cap) + r := by
+      rw [Nat.add_assoc]
+      rw [Nat.add_comm _ r]
+      rw [← Nat.add_assoc _ _ (r + cap)] 
+      rw [← Nat.succ_mul]
+    rw [h1]
+    apply add_lt_add_of_lt_of_le j.2 (Nat.le_refl r)
+
+def r_elements_of_zmodp (r d cap : Nat) 
+                        (a : Vector (Zmod p) ((d + 1) * r + (r + cap)))
+                        (hr : 1 ≤ r) : Vector (Zmod p) r := 
+  (λ (j : Fin r) => 
+    a ⟨(d + 1) + j, 
+        add_lt_add_of_le_of_lt ((le_mul_iff_one_le_right (Nat.succ_pos _)).2 hr)
+          (lt_add_of_lt_of_nonneg j.2 (Nat.zero_le _))⟩) 
+
+def helper_step (d r : Nat)
+                (a : Vector (Zmod p) ((d + 1) * r + (r + cap))) 
+                (j : Fin (d * r + (r + cap))) : ¬j.1 < (d + 1) * r → Zmod p := 
+  λ _ => a ⟨(j : Nat) + r, helper_1 d r cap j⟩
+
+def simplifications (d r cap : Nat) (a : Vector (Zmod p) ((d + 1) * r + (r + cap))) 
+                    (hr : 1 ≤ r) (j : Fin (d * r + (r + cap))) : j.val < (Nat.succ d) * r → Zmod p :=
+  λ h => 
+    a (castLt j (lt_trans h
+              ((lt_add_iff_pos_right _).2 (add_pos_of_pos_of_nonneg (Nat.pos_of_ne_zero
+                (one_le_iff_ne_zero.1 hr)) (Nat.zero_le _)))))
+
+def compose_MDS (R_f R_p r cap : Nat) (hr : 1 ≤ r) 
+                (S_box : Zmod p → Zmod p) (c : Vector (Zmod p) (r + cap)) 
+                (MDS : Matrix (Zmod p) (r + cap) (r + cap)) (k : Nat) 
+                (a : Vector (Zmod p) (k * r + (r + cap))) : Vector (Zmod p) (r + cap) :=
+/-
+TODO: we have the following error
+code generator does not support recursor 'Nat.rec' yet, 
+consider using 'match ... with' and/or structural recursion
+-/    
+  by induction k with
+    | zero => 
+        rw [Nat.zero_mul] at a 
+        rw [Nat.zero_add] at a;
+        refine λ i => P_perm p (r + cap) R_p R_f S_box c a MDS i
+    | succ d hd => 
+        refine (λ i => P_perm p (r + cap) R_p R_f S_box c
+                         (add_to_state p (r + cap) r (r_elements_of_zmodp p r d cap a hr) 
+                            (hd (λ j => dite ((j : ℕ) < (.succ d) * r) 
+                                          (simplifications p d r cap a hr j) 
+                                          (helper_step p d r a j)
+                                          )))
+                             MDS 
+                             i)
+
+/- The Poseidon hash function, takes `N` bits and returns `o` `𝔽_p`-elements. -/
+def P_hash (R_f R_p r o cap : Nat) (hr : 1 ≤ r) (S_box : Zmod p → Zmod p) 
+  (c : Vector (Zmod p) (r + cap))
+  (MDS : Matrix (Zmod p) (r + cap) (r + cap)) (ho : o < r + cap)
+  (k : Nat) (a : Vector (Zmod p) (k * r + (r + cap))) : Vector (Zmod p) o :=
+  @Function.comp (Fin o) (Fin (r + cap)) (Zmod p)
+    (compose_MDS p R_f R_p r cap hr S_box c MDS k a)
+    (fin_coercion ho)
 end Unsafe
--- Poseidon/Util/Util.lean
+++ Poseidon/Util/Util.lean
@@ -1,5 +1,36 @@
+import Mathlib.Init.Algebra.Order
+
 namespace Util
 
 def castLt (i : Fin m) (h : i.1 < n) : Fin n := ⟨i.1, h⟩
+
+instance : Preorder Nat where
+  le_refl := sorry
+  le_trans := sorry
+  lt_iff_le_not_le := sorry
+
+section Nat
+
+theorem add_lt_add_of_lt_of_le 
+  {a b c d : Nat} (h₁ : a < b) (h₂ : c ≤ d) : a + c < b + d := sorry
+
+theorem add_lt_add_of_le_of_lt 
+  {a b c d : Nat} (h₁ : a ≤ b) (h₂ : c < d) : a + c < b + d := sorry
+
+theorem le_mul_iff_one_le_right
+  {a b : Nat} : 0 < b → (b ≤ b * a ↔ 1 ≤ a) := sorry
+
+theorem lt_add_of_lt_of_nonneg
+  {a b c : Nat} : b < c → 0 ≤ a → b < c + a := sorry
+
+theorem lt_add_iff_pos_right
+  (a : Nat) {b : Nat} : a < a + b ↔ 0 < b := sorry
+
+theorem add_pos_of_pos_of_nonneg
+  {a b : Nat} : 0 < a → 0 ≤ b → 0 < a + b := sorry
+
+theorem one_le_iff_ne_zero {n : Nat} : 1 ≤ n ↔ n ≠ 0 := sorry
+
+end Nat
 
 end Util
