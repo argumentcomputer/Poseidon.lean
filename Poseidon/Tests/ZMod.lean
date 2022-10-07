363cae360c5dba291acfcb4fd182c4c18356ef7b
mpenciak <matej.penciak@gmail.com>
2022-10-06 22:22:55 -0400

    use new slimcheck notation to compose tests

--- Poseidon/Tests/ZMod.lean
+++ Poseidon/Tests/ZMod.lean
@@ -14,15 +14,8 @@ def zModGen {n : Nat} : Gen (Zmod n) := return (← Gen.choose Nat 0 n.pred)
 
 instance {n : Nat} : SampleableExt (Zmod n) := mkSelfContained zModGen
 
--- TODO : Merge PR in `LSpec` that fixes composable Slimcheck checks. 
-
 #lspec
-  check "ZMod add comm" <| ∀ n : Nat, ∀ a b : Zmod n, (a + b mod n) = (b + a mod n)
-
-#lspec
-  check "ZMod mul comm" <| ∀ n : Nat, ∀ a b : Zmod n, (a * b mod n) = (b * a mod n)
-
--- TODO : Figure out a clever way to not restrict to a particular prime
-#lspec
-  check "Zmod inv works" <| ∀ a : Zmod 7681, a ≠ 0 → (a / a = 1)
+  check "ZMod add comm" (∀ n : Nat, ∀ a b : Zmod n, (a + b mod n) = (b + a mod n)) $
+  check "ZMod mul comm" (∀ n : Nat, ∀ a b : Zmod n, (a * b mod n) = (b * a mod n)) $
+  check "Zmod inv works" (∀ a : Zmod 7681, a ≠ 0 → (a / a = 1))
   
