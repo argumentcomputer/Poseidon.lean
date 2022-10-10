fba44f0325ba9a05a11cb2e96ab3c129e6a03f14
mpenciak <matej.penciak@gmail.com>
2022-10-10 16:17:09 -0400

    make profiles generic

---
+++ Poseidon/Util/PoseidonLurk.lean
@@ -0,0 +1,12 @@
+import YatimaStdLib.Matrix
+import YatimaStdLib.Zmod
+
+namespace lurk_poseidon_blstrs
+
+/- The blstrs prime parameter -/
+def p := 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001
+
+/- Security `128` bit security parameter -/
+def M := 128
+
+end lurk_poseidon_blstrs
