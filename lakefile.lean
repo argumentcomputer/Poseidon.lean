dcbc1f112581c11600415478dec0283a2b18a465
mpenciak <matej.penciak@gmail.com>
2022-10-20 11:08:47 -0400

    add license and bump  yatimastdlib

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package Poseidon
 lean_lib Poseidon
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "1dc0ee2cb472ae011f71e1634c8ade4e8b2445c9"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "cbf5cd7c098c4369d93b9b8399a323bf0a28c107"
 
 require LSpec from git
    "https://github.com/yatima-inc/LSpec" @ "9c9f3cc9f3148c1b2d6071a35e54e4c5392373b7"
