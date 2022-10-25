d67f75c251e5952d67d8181907aa431c41438cd8
mpenciak <matej.penciak@gmail.com>
2022-10-25 00:10:06 -0400

    profiles docs

--- Poseidon/Profile.lean
+++ Poseidon/Profile.lean
@@ -1,7 +1,26 @@
 import YatimaStdLib.Zmod
 
+/-!
+# Poseidon Profiles
+
+This module defines _Profiles_ used by the Poseidon hash function
+
+A **security profile** consists of 
+* `M` : The number of bits that can be securely hashed
+* `t` : The internal width of the hashing function
+* `p` : The prime characteristic of the base field
+* `a` : The s box exponent
+
+A **hash profile** has the additional data of
+* `fullRounds` : The number of full rounds the hash function runs with this profile
+* `partRounds` : The number of partial rounds run the hash function runs with this profile
+-/
+
 namespace Poseidon
 
+/--
+The Poseidon security profile, contains data about secure bit lengths, width, primes, and s box exponent
+-/
 structure SecProfile where
   M : Nat -- Security 
   t : Nat -- Internal capacity
@@ -9,6 +28,11 @@ structure SecProfile where
   a : Int -- SBox exponent
 deriving Repr
 
+/--
+The Poseidon hash profile, contains data about the number of full and partial rounds the Hash should run.
+A minimally secure `HashProfile` can be derived from `SecProfile` using `SecProfile.hashProfile` found
+in `Poseidon.RoundNumbers`.
+-/
 structure HashProfile extends SecProfile where
   fullRounds : Nat
   partRounds : Nat
