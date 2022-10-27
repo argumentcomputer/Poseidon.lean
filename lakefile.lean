7dae1f42cb79d6fff4cbf6849a63c23319611b1f
mpenciak <matej.penciak@gmail.com>
2022-10-27 18:27:47 -0400

    bump toolchain to 2022-10-27

--- lakefile.lean
+++ lakefile.lean
@@ -3,14 +3,14 @@ open Lake DSL
 
 package Poseidon 
 
-@[defaultTarget]
+@[default_target]
 lean_lib Poseidon
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "11336e502174b1449f525b1e44d76f6a44fbd274"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "2b914196a8c67838e63c1c1e44eaf339b8a50eb7"
 
 require LSpec from git
-   "https://github.com/yatima-inc/LSpec" @ "9c9f3cc9f3148c1b2d6071a35e54e4c5392373b7"
+   "https://github.com/yatima-inc/LSpec" @ "02e423d02d2ba1b76bed3cf6459a5c2d7a13afb8"
 
 -- Tests
 lean_exe Tests.RoundNumbers
