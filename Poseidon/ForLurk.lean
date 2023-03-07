755aeb7036bf06ff4a0a4241423b2f19157b2915
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2023-03-06 21:35:13 -0500

    Add Arity 3 hashing (#18)
    * arity 3 hash

--- Poseidon/ForLurk.lean
+++ Poseidon/ForLurk.lean
@@ -1,5 +1,6 @@
 import Poseidon.HashImpl
 import Poseidon.Parameters.Lurk
+import Poseidon.Parameters.Lurk3
 
 /-!
 # Parameters for Lurk Profile
@@ -10,15 +11,22 @@ context, and input formatting to mirror the Poseidon hashing used in Lurk.
 
 namespace Poseidon.Lurk
 
+open Poseidon 
+
 abbrev F := Zmod Lurk.Profile.p
 
+def Context3 : Hash.Context Lurk3.Profile := 
+  ⟨Lurk3.MDS, Lurk3.roundConstants⟩
+
 /-- The pre-computed hashing context used by Lurk. -/
-def Context : Hash.Context Profile :=
+def Context4 : Hash.Context Profile :=
   ⟨Lurk.MDS, Lurk.roundConstants⟩
 
+def hash3 (f₁ f₂ f₃ : F) : F :=
+  Poseidon.hash Lurk3.Profile Context3 #[f₁, f₂, f₃] .merkleTree
 /--
 The hashing function used by Lurk that uses pre-initialized Lurk parameters and
 constants.
 -/
-def hash (f₁ f₂ f₃ f₄ : F) : F :=
-  Poseidon.hash Profile Context #[f₁, f₂, f₃, f₄] .merkleTree
+def hash4 (f₁ f₂ f₃ f₄ : F) : F :=
+  Poseidon.hash Lurk.Profile Context4 #[f₁, f₂, f₃, f₄] .merkleTree
